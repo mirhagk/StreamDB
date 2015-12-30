81f739b3abef6e4e5c577cbbfab719fc18d7119e
mirhagk <mirhagk@gmail.com>
2015-12-30 14:22:38 -0500

    Added auto sync switch

--- StreamDB/StreamDB/Database.cs
+++ StreamDB/StreamDB/Database.cs
@@ -8,13 +8,21 @@ namespace StreamDB
 {
     public class Database
     {
+        public bool AutoSync { get; set; } = true;
         public Database(IDatabaseConnection connection)
         {
             Connection = connection;
         }
         public void QueueUpdate<T>(T item)
         {
+            if (AutoSync)
+            {
 
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
         private IDatabaseConnection Connection { get; }
         public void SaveChanges()
