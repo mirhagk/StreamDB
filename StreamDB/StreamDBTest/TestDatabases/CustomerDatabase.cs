c9ca496e5f8bfaa300f40548d081ae83f531f3b9
mirhagk <mirhagk@gmail.com>
2015-12-30 18:00:30 -0500

    Fixed numbering of protomembers

--- StreamDB/StreamDBTest/TestDatabases/CustomerDatabase.cs
+++ StreamDB/StreamDBTest/TestDatabases/CustomerDatabase.cs
@@ -11,13 +11,13 @@ namespace StreamDBTest.TestDatabases
     [ProtoContract]
     class Person : ITableItem
     {
-        [ProtoMember(0)]
-        public int RowVersion { get; set; }
         [ProtoMember(1)]
-        public Guid RowGuid { get; set; }
+        public int RowVersion { get; set; }
         [ProtoMember(2)]
-        public string FirstName { get; set; }
+        public Guid RowGuid { get; set; }
         [ProtoMember(3)]
+        public string FirstName { get; set; }
+        [ProtoMember(4)]
         public string LastName { get; set; }
         
     }
